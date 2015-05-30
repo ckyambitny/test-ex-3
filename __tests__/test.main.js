e5ac55224f0498cf001430c94a9ee31afcbfda37
Piotr Kowalski <piecioshka@gmail.com>
2015-05-30 17:15:42 +0200

    Add tests.

---
+++ __tests__/test.main.js
@@ -0,0 +1,31 @@
+jest.dontMock('../Animal');
+
+var Animal = require('../Animal');
+var Tiger = require('../Tiger');
+var Elephant = require('../Elephant');
+
+describe('General', function () {
+    it('should list of constructor exists', function () {
+        expect(typeof Animal).toEqual('function');
+        expect(typeof Tiger).toEqual('function');
+        expect(typeof Elephant).toEqual('function');
+    });
+});
+
+describe('Animal', function () {
+    it('Animal should have own prototype', function () {
+        expect(Animal.prototype).not.isEqual({});
+    });
+});
+
+describe('Tiger', function () {
+    it('Animal should have own prototype', function () {
+        expect(Tiger.prototype).not.isEqual({});
+    });
+});
+
+describe('Elephant', function () {
+    it('Animal should have own prototype', function () {
+        expect(Elephant.prototype).not.isEqual({});
+    });
+});
