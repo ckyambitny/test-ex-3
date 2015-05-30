5fc2188963f263fdca59ec6186a5e7b03ab2eeef
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-05-30 20:26:13 +0200

    added console logs

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 var Tiger = require('./Tiger');
 var Elephant = require('./Elephant');
 
+var tigers = [];
+tigers.push(new Tiger('jan', 2, 5));
+tigers.push(new Tiger ('kkk', 5, 1));
+tigers.push(new Tiger('tygr3', 7, 4));
 
-var tigr1 = new Tiger('jan', 2, 5),
-    tigr2 = new Tiger ('kkk', 5, 1), 
-    tigr3 = new Tiger('tygr3', 7, 4);
-
-var ele1 = new Elephant('el1', 3, 4),
-    ele2 = new Elephant('el2', 2, 2),
-    ele3 = new Elephant('ele3', 4, 8);
+console.log(tigers);
+var elephants =[];
+elephants.push(new Elephant('el1', 3, 4));
+elephants.push(new Elephant('el2', 2, 2));
+elephants.push(new Elephant('ele3', 4, 8));
+console.log(elephants);
